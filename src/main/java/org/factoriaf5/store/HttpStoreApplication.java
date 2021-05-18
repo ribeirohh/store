91d59c9649d19abe182a55ffe7aecb61e505c87e
Amanda <amandaribeirinho@outlook.es>
2021-05-18 09:54:04 +0200

    store

--- src/main/java/org/factoriaf5/store/HttpStoreApplication.java
+++ src/main/java/org/factoriaf5/store/HttpStoreApplication.java
@@ -1,13 +1,13 @@
-package com.example.store;
+package org.factoriaf5.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Factoriaf5Application {
+public class HttpStoreApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Factoriaf5Application.class, args);
+        SpringApplication.run(HttpStoreApplication.class, args);
     }
 
 }
--- src/test/java/org/factoriaf5/store/Factoriaf5ApplicationTests.java
+++ src/test/java/org/factoriaf5/store/Factoriaf5ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.store;
+package org.factoriaf5.store;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
